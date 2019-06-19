0c0afe726ed92e7f0aedd956b396d16260cbcc9f
Leonel <leonelaf12@gmail.com>
2019-06-19 16:36:13 -0400

    testing for Dashboard and Display done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard /> ', () => {
+   
+    it('matches snapshot', () => {
+        const { container } = render(<Dashboard />);
+        expect(container).toMatchSnapshot();
+    })
+
+    it('renders all children', () => {
+        render(<Dashboard />);
+
+        expect(document.querySelector('.controls')).toBeTruthy();
+        expect(document.querySelector('.display')).toBeTruthy();
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,44 @@
-// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+
+import Display from './Display';
+import Dashboard from '../dashboard/Dashboard';
+
+describe('<Display />', () => {
+    it('matches snapshot', () => {
+        const { container } = render(<Display />);
+        expect(container).toMatchSnapshot();
+    })
+
+    it('displays unlocked by default', () => {
+        const { getByText} = render(<Display />);
+
+        getByText('Unlocked');
+    });
+
+    it('displays open by default', () => {
+        const { getByText } = render(<Display />);
+        getByText('Open')
+    })
+
+    it('displays locked when locked', () => {
+        const { getByText } = render(<Display locked = {true} />);
+        getByText('Locked')
+    })
+
+    it('displays unlocked when unlocked', () => {
+        const { getByText } = render(<Display locked = {false} />);
+        getByText('Unlocked')
+    })
+
+    it('displays open when opened', () => {
+        const { getByText } = render(<Display open = {true}/>);
+        getByText('Open')
+    })
+
+    it('displays closed when closed', () => {
+        const { getByText } = render(<Display closed = {true}/>);
+        getByText('Closed')
+    })
+})
